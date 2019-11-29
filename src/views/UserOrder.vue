23f61bffbd5405dcecfdbaeb08d46c8b903b5813
Shachal Ami-Ad <shachalamiad@hotmail.com>
2019-11-29 13:34:48 +0200

    reserve box

--- src/views/UserOrder.vue
+++ src/views/UserOrder.vue
@@ -6,8 +6,22 @@
         <div v-if="houseData.name" class="houseGeneralInfo">
         <h1 class="houseTitle">{{houseData.name}} </h1>
         <h2> Your host: {{houseData.hostId}}</h2>
-  
         </div>
+        <img v-if="houseData.imgs" class="houseMainImage" :src="houseData.imgs[0]" />
+        <section class="reserve-form"> 
+          <div class="reserve-form-container">
+            <p class="reserve-form-price"><span class="price">{{houseData.price}}$</span> /per night</p>
+            <p class="reviews-data"> <span class="bold">*4.89</span> (299 reviews) </p>
+            <hr class="reserve-form-hr">
+            <p class="reviews-data-bold">Dates</p>
+            <div class="reserve-form-boxes-container flex space-between align-center">
+              <div class="reserve-form-boxes"><span class="text-margin">Check-in</span></div>
+              <div class="reserve-form-boxes"><span class="text-margin">Check-out</span></div>
+
+            </div>
+          </div>
+        </section>
+        
     </section>
 </template>
 
