7d4fc001a044f41b20ec81f086ea00cb27f00722
hosam faour <hosamf1992@gmail.com>
2019-11-29 23:15:34 +0200

    check dates calendar

--- src/components/SearchForm.vue
+++ src/components/SearchForm.vue
@@ -1,40 +1,67 @@
 <template name="searchForm">
   <form class="searchForm">
-      <slot></slot>
+    testing dates!!
+    available dates example: 15/12/2019 - 20/12/2019
+    <div>user from : {{from}}</div>
+    user to : {{to}}
+    <div> Res:{{res}}</div>
+    <slot></slot>
+    
     <div class="where">
       Country / State:
       <input type="text" />
     </div>
     <div class="when">
       From:
-      <input type="date" />
+      <input v-model="from" type="date" />
       To:
-      <input type="date" />
+      <input v-model="to" type="date" />
     </div>
+   
     <div class="howMany">
       How Many Visitors:
       <select>
-          <option value="few">1-3</option>
-          <option value="middle">4-7</option>
-          <option value="many">8+</option>
+        <option value="few">1-3</option>
+        <option value="middle">4-7</option>
+        <option value="many">8+</option>
       </select>
     </div>
-    <input type="submit" class="submit" value="TRAVEL" @click.prevent="doSearch">
+    <button @click.prevent="calcDateRange">check Results</button>
+    <input type="submit" class="submit" value="TRAVEL" @click.prevent="doSearch" />
   </form>
 </template>
 
 <script>
 export default {
-    methods: {
-        doSearch(ev) {
-            const where = ev.target.parentElement.children[1].children[0].value
-            const when = {}
-            when.from = ev.target.parentElement.children[2].children[0].value
-            when.until = ev.target.parentElement.children[2].children[1].value
-            const who = ev.target.parentElement.children[3].children[0].value
-            const searchBy = {when, where, who}
-            this.$router.push('/house')
-        }
+  data() {
+    return {
+      from: "",
+      to: "",
+      res: ""
+    };
+  },
+  methods: {
+    doSearch(ev) {
+      const where = ev.target.parentElement.children[1].children[0].value;
+      const when = {};
+      when.from = ev.target.parentElement.children[2].children[0].value;
+      when.until = ev.target.parentElement.children[2].children[1].value;
+      const who = ev.target.parentElement.children[3].children[0].value;
+      const searchBy = { when, where, who };
+      this.$router.push("/house");
+    },
+    calcDateRange() {
+      var startdate = new Date("2019/12/15").toLocaleDateString();
+      var enddate = new Date("2019/12/20").toLocaleDateString();
+      var startD = new Date(this.from).toLocaleDateString();
+      var endD = new Date(this.to).toLocaleDateString();
+      if (startD > endD) return this.res='enter valid date ';
+      if (startD >= startdate && endD <= enddate) {
+        this.res="Yes sure!!"
+      } else {
+        this.res="No place!!"
+      }
     }
+  }
 };
 </script>
