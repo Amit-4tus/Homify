f79b294b4132e6500a5fa2e79c24c8ab7bfd03ad
hosam faour <hosamf1992@gmail.com>
2019-12-02 10:43:33 +0200

    zoom map

--- src/components/GMap.vue
+++ src/components/GMap.vue
@@ -1,10 +1,17 @@
 <template>
-  <section  class="listMap">
+  <section v-if="showMap" class="listMap">
     <!-- {{isFixed}} -->
-    <GmapMap v-if="center"  ref="mapRef" :center="center" :zoom="10" map-type-id="terrain" style="width: 100%; height: 100%">
-    <GmapMarker
-   :position="markers" :clickable="true" @click="center=m.position"
-  />
+    <GmapMap
+      v-if="center"
+      ref="mapRef"
+      :center="center"
+      :zoom="zoom"
+      map-type-id="terrain"
+      style="width: 100%; height: 100%"
+    >
+      <GmapMarker v-for="(m, index) in markers"
+       @click="zoomIn"
+        :key="index" :position="m" :visible="m.visibility" />
     </GmapMap>
   </section>
 </template>
@@ -13,32 +20,38 @@
 export default {
   props: {
     coords: Object,
-    marker:Array
+    marker: Array
   },
   data() {
     return {
-      // markers: { lat: 32.0852999, lng: 34.78176759999999 },
+      // markers: [{ lat: 32.0852999, lng: 34.78176759999999 }],
       // center: { lat: 32.0852999, lng: 34.78176759999999 },
-      markers: null,
+      markers: [],
       center: null,
-      showMap:false
-      // center:this.coords
+      showMap: false,
+      zoom:7
     };
   },
-  methods: {
-    
-  },
+  methods: {},
   created() {
     this.center = this.coords;
-    this.markers = this.coords;
-   
+    this.markers=this.marker;
+
     // console.log(document.querySelector('.listMap')).offsetTop
   },
- async mounted() {
-    // if(await this.marker.length>0){
-    //   console.log(this.marker)
-    //   this.showMap=true;
-    // }
+  methods:{
+zoomIn(){
+  this.zoom=13
+}
+  },
+  async mounted() {
+    if (!this.marker) {
+      let temp = [];
+      temp.push(this.coords);
+      this.markers = temp;
+      this.showMap = true;
+    }
+    this.showMap = true;
   },
   computed: {
     isFixed() {
@@ -46,11 +59,6 @@ export default {
       // if ()
     }
   },
-  watch: {
-    coords() {
-      this.center = this.coords;
-      this.markers = this.coords;
-    }
-  }
+  watch: {}
 };
 </script>
