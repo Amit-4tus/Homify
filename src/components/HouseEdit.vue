7c51178e17449612eb3c53b88b51240c04d7ab83
Shachal Ami-Ad <shachalamiad@hotmail.com>
2019-12-03 21:26:51 +0200

    add house

--- src/components/HouseEdit.vue
+++ src/components/HouseEdit.vue
@@ -1,110 +1,122 @@
 <template >
   <div>
-    <section class="add-house-page">
+    <section class="add-house-page flex flex-row space-between">
+      <section class="add-house-page-left">
+        <section class="add-house-step-container">
+          <div class="add-house-welcome-text-1">Hi, __________! Let's get started</div>
+          <div class="add-house-welcome-text-2">listing your space</div>
 
+          <p class="add-house-step bold">step 1</p>
 
-        <section class="add-house-page-left">
+          <p class="add-house-name-of-house-text">How would you like to name your place?</p>
+          <input class="add-house-name-input" v-model="newHouse.name" type="text" maxlength="50" />
 
-          <section class="add-house-step-container">
+          <p class="add-house-address-text">Where is your place located?</p>
+          <div class="add-house-location-container">
+            <input
+              class="add-house-address-input"
+              @change="getCoords"
+              v-model="newHouse.location.address.country"
+              type="text"
+              placeholder="Country"
+            />
+            <input
+              class="add-house-address-input"
+              @change="getCoords"
+              v-model="newHouse.location.address.city"
+              type="text"
+              placeholder="City"
+            />
 
-              <div class="add-house-welcome-text-1">Hi, __________! Let's get started</div>
-              <div class="add-house-welcome-text-2">listing your space</div>
-
-              <p class="add-house-step bold">step 1</p>
-
-              <p class="add-house-name-of-house-text">How would you like to name your place?</p>
-              <input class="add-house-name-input" v-model="newHouse.name" 
-                      type="text" maxlength="50"/>
-
-              <p class="add-house-address-text">Where is your place located?</p>
-              <div class="add-house-location-container">
-                <input class="add-house-address-input" @change="getCoords" 
-                        v-model="newHouse.location.address.country" type="text" placeholder="Country"/>
-          
-                <input class="add-house-address-input" @change="getCoords" 
-                        v-model="newHouse.location.address.city" type="text" placeholder="City"/>
-            
-                <input class="add-house-address-input" @change="getCoords" 
-                        v-model="newHouse.location.address.street" type="text" placeholder="Street"/>
-              </div>
-          </section>
-        
-          <section class="add-house-step-container flex-column">
-
-              <p class="add-house-step bold">step 2</p>
-              <p class="add-house-charge-per-night-text">What will be the Dollar cost per night?</p>
-
-              <input class="add-house-price-input" v-model="newHouse.price" type="number"/>
-
-              <p class="add-house-amenities-text">What amenities do you offer?</p>
-                <el-select class="add-house-amenities-input" v-model="newHouse.amentities" multiple placeholder="Please select">
-                <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value"></el-option>
-                </el-select>
-
-              <p class="add-house-step bold">step 3</p>
-
-              <p class="add-house-Upload-image-text">Show us how does your place look like?</p>
-              
-              
-              <section class="upload-btns-container ">
-                
-                <el-upload
-                  action
-                  submit="submit"
-                  :auto-upload="false"
-                  :on-preview="handlePreview"
-                  :on-remove="handleRemove"
-                  :show-file-list="true"
-                  :before-remove="beforeRemove"
-                  multiple
-                  :limit="3"
-                  :on-exceed="handleExceed"
-                  :on-change="onChange"
-                >
-                <el-button class="upload-imgs-btn bold" size="x-large" type="primary">Add images</el-button>
-                <div slot="tip" class="el-upload__tip">jpg/png files with a size less than 500kb</div>
-                </el-upload>
-                  <el-button v-if="fileList.length" class="upload-imgs-btn 2 bold"
-                      size="x-large"
-                      type="success"
-                      @click="submitUpload"
-                    >Upload images</el-button>
-                <div v-for="img in newHouse.imgs" :key="img.id">
-                  <img class="img-present" v-if="newHouse.imgs" :src="img" />
-                </div>
-            </section>
-
-            <button v-if="isEditing" @click="updateHouse">Update</button>
-            <button v-if="isEditing" @click="deleteHouse">Delete</button>
-          </section>
+            <input
+              class="add-house-address-input"
+              @change="getCoords"
+              v-model="newHouse.location.address.street"
+              type="text"
+              placeholder="Street"
+            />
+          </div>
         </section>
 
+        <section class="add-house-step-container flex-column">
+          <p class="add-house-step bold">step 2</p>
+          <p class="add-house-charge-per-night-text">What will be the Dollar cost per night?</p>
+
+          <input class="add-house-price-input" v-model="newHouse.price" type="number" />
+
+          <p class="add-house-amenities-text">What amenities do you offer?</p>
+          <el-select
+            class="add-house-amenities-input"
+            v-model="newHouse.amentities"
+            multiple
+            placeholder="Please select"
+          >
+            <el-option
+              v-for="item in options"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value"
+            ></el-option>
+          </el-select>
+
+          <p class="add-house-step bold">step 3</p>
+          <div class="calender-container flex flex-row align-center space-between">
+            <div class="check-in">
+              <p class="check-txt">Check-in</p>
+              <input class="check-input" :min="dateMin" v-model="newHouse.dates.from" type="date" />
+            </div>
+            <div class="check-out">
+              <p class="check-txt">Check-out</p>
+              <input class="check-input" :min="dateMin" v-model="newHouse.dates.to" type="date" />
+            </div>
+          </div>
+
+          <p class="add-house-step bold">step 4</p>
+          <p class="add-house-Upload-image-text">Show us how does your place look like?</p>
+          <section class="upload-btns-container">
+            <el-upload
+              action
+              submit="submit"
+              :auto-upload="false"
+              :on-preview="handlePreview"
+              :on-remove="handleRemove"
+              :show-file-list="true"
+              :before-remove="beforeRemove"
+              multiple
+              :limit="3"
+              :on-exceed="handleExceed"
+              :on-change="onChange"
+            >
+              <el-button class="upload-imgs-btn bold" size="x-large" type="primary">Add images</el-button>
+              <div slot="tip" class="el-upload__tip">jpg/png files with a size less than 500kb</div>
+            </el-upload>
+            <el-button
+              v-if="fileList.length"
+              class="upload-imgs-btn 2 bold"
+              size="x-large"
+              type="success"
+              @click="submitUpload"
+            >Upload images</el-button>
+          </section>
+          <section class="add-house-imgs-container flex flex-row justify-center space-between wrap">
+            <div v-for="img in newHouse.imgs" :key="img.id">
+              <img class="img-present" v-if="newHouse.imgs" :src="img" />
+            </div>
+          </section>
+
+          <button class="add-house-btn" v-if="!isEditing" @click="addHouse">Add House</button>
+          <button class="add-house-btn" v-if="isEditing" @click="updateHouse">Update</button>
+          <button class="add-house-btn" v-if="isEditing" @click="deleteHouse">Delete</button>
+        </section>
+      </section>
 
       <section class="add-house-page-right">
-        
-        <div class="add-house-step-container">
-          <p class="add-house-step-4 bold">step 4</p>
-          <div class="calender-container flex flex-row align-center space-between">
-              <div class="check-in">
-                <p class="check-txt">Check-in</p>
-                <input class="check-input" :min="dateMin" v-model="newHouse.dates.from" type="date" />
-              </div>
-              <div class="check-out">
-                <p class="check-txt">Check-out</p>
-                <input class="check-input" :min="dateMin" v-model="newHouse.dates.to" type="date" />
-              </div>
-              </div>
-            </div>
-          <button class="add-house-btn" @click="addHouse">Add House</button>
-          <div>    
-         <img class="appartment-draw"  src="../assets/imgs/appartment.jpg"/>
-         </div>
-
-
+        <img class="appartment-draw" src="../assets/imgs/appartment.jpg" />
       </section>
     </section>
+
     <!-- <h2>description</h2>
-    <textarea v-model="newHouse.desc"></textarea> -->
+    <textarea v-model="newHouse.desc"></textarea>-->
   </div>
 </template>
 
@@ -135,9 +147,7 @@ export default {
       this.newHouse = JSON.parse(JSON.stringify(currHouse));
     }
   },
-  computed: {
-      
-  },
+  computed: {},
   methods: {
     handleRemove(file, fileList) {
       var filterd = this.fileList.filter(img => img.uid !== file.uid);
@@ -164,7 +174,7 @@ export default {
         })
       );
       await imgUrls.forEach(img => {
-        this.newHouse.imgs.push(img.url)
+        this.newHouse.imgs.push(img.url);
         // console.log(this.newHouse.imgs[0])
       });
       this.doneUpload = true;
@@ -218,12 +228,12 @@ export default {
             street: "",
             city: "",
             country: ""
-          },
-        },
-         reviews: {
-            avgRating: 4.8,
-            reviewsIds: []
           }
+        },
+        reviews: {
+          avgRating: 4.8,
+          reviewsIds: []
+        }
       };
     },
     amenitiesOps() {
@@ -252,7 +262,6 @@ export default {
     }
   },
 
- 
   computed: {
     dateMin() {
       var d = new Date(new Date()),
