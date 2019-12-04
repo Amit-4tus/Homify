45f0370ec8cdfd437d0987f7a7f5cb88a0dccf14
Shachal Ami-Ad <shachalamiad@hotmail.com>
2019-12-04 11:18:20 +0200

    houses

--- src/components/ReviewPreview.vue
+++ src/components/ReviewPreview.vue
@@ -1,6 +1,6 @@
 <template>
   <section>
-    <h2>From: {{itemData.user.userName}}</h2>
+    <h2>From:  {{itemData.user.userName}}</h2>
     <h3>Review: {{itemData.txt}}</h3>
     <h4>Rating: {{itemData.rating}}</h4>
     <h4>CreatedAt: {{itemData.createdAt}}</h4>
