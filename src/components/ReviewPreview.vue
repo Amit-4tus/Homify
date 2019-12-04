f7f28d57a1d73c7ef60617843b32ffaef9ba1253
Amit Fortus <amit.fortus4@gmail.com>
2019-12-04 17:06:07 +0200

    add house page designed

--- src/components/ReviewPreview.vue
+++ src/components/ReviewPreview.vue
@@ -1,9 +1,9 @@
 <template>
   <section class="reviewPreview">
-    <h2>By: {{itemData.user.userName}}</h2>
+    <h3>By: {{itemData.user.userName}}</h3>
     <h3>Review: {{itemData.txt}}</h3>
     <h3>star{{itemData.rating}}</h3>
-    <h4>Written On: {{itemData.createdAt}}</h4>
+    <h3>Written On: {{itemData.createdAt}}</h3>
   </section>
 </template>
 
